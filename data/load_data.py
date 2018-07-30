9b6a06ab48693a96cbaae46c3b72f79ad810abbb
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-30 17:01:36 +0800

    sync py code

--- data/load_data.py
+++ data/load_data.py
@@ -1,14 +1,13 @@
 import numpy as np
-from data.omniglot import Omniglot
 
 
 def load(dataset_name, **kwargs):
     if dataset_name == 'gpsamples':
-        load_gpsamples(**kwards)
+        return load_gpsamples(**kwards)
     elif dataset_name == 'sinusoid':
-        load_sinusoid(**kwards)
+        return load_sinusoid(**kwards)
     elif dataset_name == 'omniglot':
-        load_omniglot(**kwargs)
+        return load_omniglot(**kwargs)
     else:
         raise Exception("Dataset {0} not found".format(dataset_name))
 
@@ -20,6 +19,7 @@ def load_sinusoid():
     from data.sinusoid import Sinusoid
     train_set = Sinusoid(amp_range=[0.1, 5.0], phase_range=[0, np.pi], period_range=[2*np.pi, 2*np.pi], input_range=[-5., 5.], dataset_name=args.dataset_name)
     val_set = train_set
+    return train_set, val_set
 
 def load_gpsamples():
     from data.gpsample import GPSample
@@ -28,3 +28,4 @@ def load_gpsamples():
     val_data = {"xs":data['xs'][50000:60000], "ys":data['ys'][50000:60000]}
     train_set = GPSample(input_range=[-2., 2.], var_range=[0.5, 0.5], max_num_samples=200, data=train_data)
     val_set = GPSample(input_range=[-2., 2.], var_range=[0.5, 0.5], max_num_samples=200, data=val_data)
+    return train_set, val_set
