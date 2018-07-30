f5b49ad735d1a6e8994b8c0c12ddeda8386134e0
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-30 20:23:26 +0800

    sync py code

--- run_1d_maml.py
+++ run_1d_maml.py
@@ -67,6 +67,6 @@ with tf.Session(config=config) as sess:
     if args.user_mode == 'train':
         learner.run_train(**run_params)
     elif args.user_mode == 'eval':
-        learner.run_eval(num_shots=10, test_shots=50, step=1)
-        learner.run_eval(num_shots=10, test_shots=50, step=5)
-        learner.run_eval(num_shots=10, test_shots=50, step=10)
+        learner.run_eval(eval_samples, num_shots=10, test_shots=50, step=1)
+        learner.run_eval(eval_samples, num_shots=10, test_shots=50, step=5)
+        learner.run_eval(eval_samples, num_shots=10, test_shots=50, step=10)
