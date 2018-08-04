16f3396102e5c74ef52bfecb0210be03460d524c
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-04 19:15:08 +0800

    sync py code

--- run_omniglot_maml.py
+++ run_omniglot_maml.py
@@ -39,6 +39,8 @@ model_opt = {
     "bn": False,
     "kernel_initializer": tf.contrib.layers.xavier_initializer(uniform=False),
     "kernel_regularizer":None,
+    "inner_iters": 1,
+    "eval_iters": 10,
 }
 
 model = tf.make_template('model', MAMLRegressor.construct)
@@ -48,7 +50,7 @@ for i in range(args.nr_model):
         model(models[i], **model_opt)
 
 #tags = ["test", 'small-period']
-tags = ["test"]
+tags = ["test1"]
 
 learner = MAMLLearner(session=None, parallel_models=models, optimize_op=None, train_set=train_set, eval_set=val_set, variables=tf.trainable_variables(), lr=args.learning_rate, device_type=args.device_type, tags=tags, cdir=checkpoint_dir, rdir=result_dir)
 
