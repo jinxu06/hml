a12c0c9a257170ba6b051ff3fb114e61c3e295eb
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-04 16:21:25 +0800

    sync py code

--- misc/layers.py
+++ misc/layers.py
@@ -21,7 +21,8 @@ def dense(inputs, num_outputs, W=None, b=None, nonlinearity=None, bn=False, kern
         outputs = tf.matmul(inputs, W) + tf.reshape(b, [1, num_outputs])
 
         if bn:
-            outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            # outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training)
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + dense", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
@@ -41,7 +42,8 @@ def conv2d(inputs, num_filters, W=None, b=None, filter_size=[3,3], stride=[1,1],
         outputs = tf.nn.bias_add(tf.nn.conv2d(inputs, W, [1] + stride + [1], pad), b)
 
         if bn:
-            outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            # outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training)
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + conv2d", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
@@ -68,7 +70,8 @@ def deconv2d(inputs, num_filters, W=None, b=None, filter_size=[3,3], stride=[1,1
         outputs = tf.nn.bias_add(outputs, b)
 
         if bn:
-            outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            # outputs = tf.layers.batch_normalization(outputs, training=is_training)
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training)
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + deconv2d", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
