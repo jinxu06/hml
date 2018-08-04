a33ad01ee44759afb7a269802156bbb9f0319be1
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-04 17:00:29 +0800

    sync py code

--- misc/layers.py
+++ misc/layers.py
@@ -22,7 +22,7 @@ def dense(inputs, num_outputs, W=None, b=None, nonlinearity=None, bn=False, kern
 
         if bn:
             # outputs = tf.layers.batch_normalization(outputs, training=is_training)
-            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope=name+"-BatchNorm")
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope="BatchNorm")
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + dense", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
@@ -43,7 +43,7 @@ def conv2d(inputs, num_filters, W=None, b=None, filter_size=[3,3], stride=[1,1],
 
         if bn:
             # outputs = tf.layers.batch_normalization(outputs, training=is_training)
-            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope=name+"-BatchNorm")
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope="BatchNorm")
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + conv2d", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
@@ -71,7 +71,7 @@ def deconv2d(inputs, num_filters, W=None, b=None, filter_size=[3,3], stride=[1,1
 
         if bn:
             # outputs = tf.layers.batch_normalization(outputs, training=is_training)
-            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope=name+"-BatchNorm")
+            outputs = tf.contrib.layers.batch_norm(outputs, is_training=is_training, reuse=tf.AUTO_REUSE, scope="BatchNorm")
         if nonlinearity is not None:
             outputs = nonlinearity(outputs)
         print("    + deconv2d", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
