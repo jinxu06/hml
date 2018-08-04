42c8e380268bd83e00f6dc5462264f281999014c
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-04 17:57:09 +0800

    sync py code

--- run_omniglot_maml.py
+++ run_omniglot_maml.py
@@ -36,7 +36,7 @@ model_opt = {
     "label_shape": [args.num_classes],
     "alpha": args.alpha,
     "nonlinearity": tf.nn.relu,
-    "bn": False,
+    "bn": True,
     "kernel_initializer": tf.contrib.layers.xavier_initializer(uniform=False),
     "kernel_regularizer":None,
 }
