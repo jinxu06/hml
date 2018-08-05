3204c2b1c39c5ba43b2d2b4d26a428d1b87637af
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-05 12:37:32 +0800

    sync py code

--- models/neural_processes.py
+++ models/neural_processes.py
@@ -7,6 +7,7 @@ from misc.samplers import gaussian_sampler
 from misc.helpers import int_shape, get_name, get_trainable_variables
 from misc.estimators import compute_2gaussian_kld
 from misc.losses import mean_squared_error
+from misc.metrics import accuracy
 
 
 
@@ -16,32 +17,48 @@ class NeuralProcess(object):
         self.counters = counters
         self.user_mode = user_mode
 
-    def construct(self, sample_encoder, aggregator, conditional_decoder, obs_shape, r_dim, z_dim, nonlinearity=tf.nn.relu, bn=False, kernel_initializer=None, kernel_regularizer=None):
+    def construct(self, sample_encoder, aggregator, conditional_decoder, task_type, obs_shape, r_dim, z_dim, label_shape=[], num_classes=1, nonlinearity=tf.nn.relu, bn=False, kernel_initializer=None, kernel_regularizer=None):
         #
         self.sample_encoder = sample_encoder
         self.aggregator = aggregator
         self.conditional_decoder = conditional_decoder
+        self.task_type = task_type
+        if task_type == 'classification':
+            self.error_func = tf.losses.softmax_cross_entropy
+            self.pred_func = lambda x: tf.nn.softmax(x)
+        elif task_type == 'regression':
+            self.error_func = tf.losses.mean_squared_error
+            self.pred_func = lambda x: x
+        else:
+            raise Exception("Unknown task type")
         self.obs_shape = obs_shape
         self.r_dim = r_dim
         self.z_dim = z_dim
+        self.label_shape = label_shape
+        self.num_classes = num_classes
         self.nonlinearity = nonlinearity
         self.bn = bn
         self.kernel_initializer = kernel_initializer
         self.kernel_regularizer = kernel_regularizer
         #
         self.X_c = tf.placeholder(tf.float32, shape=tuple([None,]+obs_shape))
-        self.y_c = tf.placeholder(tf.float32, shape=(None,))
+        self.y_c = tf.placeholder(tf.float32, shape=tuple([None,]+label_shape))
         self.X_t = tf.placeholder(tf.float32, shape=tuple([None,]+obs_shape))
-        self.y_t = tf.placeholder(tf.float32, shape=(None,))
+        self.y_t = tf.placeholder(tf.float32, shape=tuple([None,]+label_shape))
+
         self.is_training = tf.placeholder(tf.bool, shape=())
         self.use_z_ph = tf.cast(tf.placeholder_with_default(False, shape=()), dtype=tf.float32)
         self.z_ph = tf.placeholder_with_default(np.zeros((1, self.z_dim), dtype=np.float32), shape=(1, self.z_dim))
-        #
-        self.outputs = self._model()
-        self.y_hat = self.outputs
+
+        self._model()
         self.loss = self._loss(beta=1.0, y_sigma=0.2)
+        self.grads = tf.gradients(self.loss, tf.trainable_variables(), colocate_gradients_with_ops=True)
         #
-        self.grads = tf.gradients(self.loss, get_trainable_variables([self.scope_name]), colocate_gradients_with_ops=True)
+        # self.outputs = self._model()
+        # self.y_hat = self.outputs
+        # self.loss = self._loss(beta=1.0, y_sigma=0.2)
+        # #
+        # self.grads = tf.gradients(self.loss, get_trainable_variables([self.scope_name]), colocate_gradients_with_ops=True)
 
 
     def _model(self):
@@ -70,72 +87,133 @@ class NeuralProcess(object):
                     else:
                         raise Exception("unknown user_mode")
                     z = (1-self.use_z_ph) * z + self.use_z_ph * self.z_ph
-                    y_hat = self.conditional_decoder(self.X_t, z)
-                    return y_hat
+                    self.outputs = self.conditional_decoder(self.X_t, z)
 
     def _loss(self, beta=1., y_sigma=1./np.sqrt(2)):
         self.reg = compute_2gaussian_kld(self.z_mu_pr, self.z_log_sigma_sq_pr, self.z_mu_pos, self.z_log_sigma_sq_pos)
-        self.nll = mean_squared_error(self.y_t, self.y_hat, sigma=y_sigma)
+        # self.nll = mean_squared_error(self.y_t, self.y_hat, sigma=y_sigma)
+        self.nll = self.error_func(self.y_t, self.outputs) / (2*y_sigma**2)
         return self.nll + beta * self.reg
 
-    def predict(self, sess, X_c_value, y_c_value, X_t_value):
+    def predict(X_c_value, y_c_value, X_t_value):
         feed_dict = {
             self.X_c: X_c_value,
             self.y_c: y_c_value,
             self.X_t: X_t_value,
-            self.y_t: np.zeros((X_t_value.shape[0],)),
             self.is_training: False,
         }
-        z_mu, z_log_sigma_sq = sess.run([self.z_mu_pr, self.z_log_sigma_sq_pr], feed_dict=feed_dict)
-        z_sigma = np.exp(0.5*z_log_sigma_sq)
-        z_pr = np.random.normal(loc=z_mu, scale=z_sigma)
-        feed_dict.update({
-            self.use_z_ph: True,
-            self.z_ph: z_pr,
-        })
-        preds= sess.run(self.y_hat, feed_dict=feed_dict)
-        return preds
+        preds = self.pred_func(self.outputs)
+        return [preds], feed_dict
 
-    def manipulate_z(self, sess, z_value, X_t_value):
-        feed_dict = {
-            self.use_z_ph: True,
-            self.z_ph: z_value,
-            self.X_t: X_t_value,
-            self.is_training: False,
-        }
-        preds= sess.run(self.y_hat, feed_dict=feed_dict)
-        return preds
-
-    def compute_loss(self, sess, X_c_value, y_c_value, X_t_value, y_t_value, is_training):
+    def evaluate_metrics(X_c_value, y_c_value, X_t_value, y_t_value):
         feed_dict = {
             self.X_c: X_c_value,
             self.y_c: y_c_value,
             self.X_t: X_t_value,
             self.y_t: y_t_value,
-            self.is_training: is_training,
+            self.is_training: False,
         }
-        l = sess.run(self.loss, feed_dict=feed_dict)
-        return l
+        l = self.error_func(self.y_t, self.outputs)
+        if self.task_type == 'classification':
+            return [l, accuracy(self.y_t, self.pred_func(self.outputs))], feed_dict
+        return [l], feed_dict 
+
+
+    # def predict(self, sess, X_c_value, y_c_value, X_t_value):
+    #     feed_dict = {
+    #         self.X_c: X_c_value,
+    #         self.y_c: y_c_value,
+    #         self.X_t: X_t_value,
+    #         self.y_t: np.zeros((X_t_value.shape[0],)),
+    #         self.is_training: False,
+    #     }
+    #     z_mu, z_log_sigma_sq = sess.run([self.z_mu_pr, self.z_log_sigma_sq_pr], feed_dict=feed_dict)
+    #     z_sigma = np.exp(0.5*z_log_sigma_sq)
+    #     z_pr = np.random.normal(loc=z_mu, scale=z_sigma)
+    #     feed_dict.update({
+    #         self.use_z_ph: True,
+    #         self.z_ph: z_pr,
+    #     })
+    #     preds= sess.run(self.y_hat, feed_dict=feed_dict)
+    #     return preds
+    #
+    # def manipulate_z(self, sess, z_value, X_t_value):
+    #     feed_dict = {
+    #         self.use_z_ph: True,
+    #         self.z_ph: z_value,
+    #         self.X_t: X_t_value,
+    #         self.is_training: False,
+    #     }
+    #     preds= sess.run(self.y_hat, feed_dict=feed_dict)
+    #     return preds
+    #
+    # def compute_loss(self, sess, X_c_value, y_c_value, X_t_value, y_t_value, is_training):
+    #     feed_dict = {
+    #         self.X_c: X_c_value,
+    #         self.y_c: y_c_value,
+    #         self.X_t: X_t_value,
+    #         self.y_t: y_t_value,
+    #         self.is_training: is_training,
+    #     }
+    #     l = sess.run(self.loss, feed_dict=feed_dict)
+    #     return l
 
 
 
 
 @add_arg_scope
-def omniglot_conv_encoder(inputs, r_dim, is_training, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, counters={}):
+def omniglot_conv_encoder(X, y, num_classes, r_dim, is_training, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, counters={}):
     name = get_name("omniglot_conv_encoder", counters)
     print("construct", name, "...")
     with tf.variable_scope(name):
-        with arg_scope([conv2d, dense], nonlinearity=nonlinearity, bn=bn, kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer, is_training=is_training, counters=counters):
-            outputs = inputs
-            outputs = conv2d(outputs, 64, 3, 1, "SAME")
-            outputs = conv2d(outputs, 64, 3, 2, "SAME")
-            outputs = conv2d(outputs, 128, 3, 1, "SAME")
-            outputs = conv2d(outputs, 128, 3, 2, "SAME")
-            outputs = conv2d(outputs, 256, 4, 1, "VALID")
-            outputs = conv2d(outputs, 256, 4, 1, "VALID")
-            outputs = tf.reshape(outputs, [-1, 256])
+        default_args = {
+            "nonlinearity": nonlinearity,
+            "bn": bn,
+            "kernel_initializer": kernel_initializer,
+            "kernel_regularizer": kernel_regularizer,
+            "is_training": is_training,
+            "counters": counters,
+        }
+        num_filters = 64
+        filter_size = [3, 3]
+        stride = [2, 2]
+        with arg_scope([conv2d, dense], **default_args):
+            outputs = X
+            for _ in range(2):
+                outputs = conv2d(outputs, num_filters, filter_size, stride, "SAME")
+            y = deconv2d(tf.reshape(y, [-1, 1, 1, num_classes]), num_filters, int_shape(outputs)[1:3], [1, 1], 'VALID')
+            outputs = tf.concat([outputs, y], axis=-1)
+            for _ in range(2):
+                outputs = conv2d(outputs, num_filters, filter_size, stride, "SAME")
+            outputs = tf.reshape(outputs, [-1, num_filters])
             r = tf.dense(outputs, r_dim, nonlinearity=None, bn=False)
             return r
+
+
+@add_arg_scope
+def omniglot_conv_conditional_decoder(inputs, z, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, is_training=False, counters={}):
+    name = get_name("omniglot_conv_conditional_decoder", counters)
+    print("construct", name, "...")
+    with tf.variable_scope(name):
+        default_args = {
+            "nonlinearity": nonlinearity,
+            "bn": bn,
+            "kernel_initializer": kernel_initializer,
+            "kernel_regularizer": kernel_regularizer,
+            "is_training": is_training,
+            "counters": counters,
+        }
+        num_filters = 64
+        filter_size = [3, 3]
+        stride = [2, 2]
+        with arg_scope([conv2d, dense], **default_args):
+            outputs = inputs
+            for k in range(4):
+                outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
+            outputs = tf.reduce_mean(outputs, [1, 2])
+            outputs = tf.reshape(outputs, [-1, num_filters])
+            y = dense(outputs, num_classes, nonlinearity=None, bn=False)
+            return y
 
 
 @add_arg_scope
