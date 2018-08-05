c7daebfa02c55e2be6484ba92d773de53398b7f2
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-05 16:05:15 +0800

    sync py code

--- models/neural_processes.py
+++ models/neural_processes.py
@@ -182,7 +182,8 @@ def omniglot_conv_encoder(X, y, r_dim, num_classes, is_training, nonlinearity=No
             for _ in range(2):
                 outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
             # y = deconv2d(tf.reshape(y, [-1, 1, 1, num_classes]), num_filters, filter_size=int_shape(outputs)[1:3], stride=[1, 1], pad='VALID')
-            y = tf.tile(tf.reshape(y, [-1, 1, 1, num_classes]), multiples=tf.concat([tf.shape(outputs)[0:3], [1]], axis=0))
+            b, h, w, _ = tf.shape(outputs)
+            y = tf.tile(tf.reshape(y, [-1, 1, 1, num_classes]), tf.stack([b, h, w, 1]))
             outputs = tf.concat([outputs, y], axis=-1)
             for _ in range(2):
                 outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
@@ -212,11 +213,8 @@ def omniglot_conv_conditional_decoder(inputs, z, num_classes, nonlinearity=None,
             for _ in range(2):
                 outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
             # z = deconv2d(tf.reshape(z, [-1, 1, 1, int_shape(z)[-1]]), num_filters, filter_size=int_shape(outputs)[1:3], stride=[1, 1], pad='VALID')
-            print(int_shape(outputs))
-            print(int_shape(z))
-            z = tf.tile(tf.reshape(z, [1, 1, 1, int_shape(z)[-1]]), multiples=tf.concat([tf.shape(outputs)[0:3], [1]], axis=0))
-            print(int_shape(outputs))
-            print(int_shape(z))
+            b, h, w, _ = tf.shape(outputs)
+            z = tf.tile(tf.reshape(z, [1, 1, 1, int_shape(z)[-1]]), tf.stack([b, h, w, 1]))
             outputs = tf.concat([outputs, z], axis=-1)
             for _ in range(2):
                 outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
