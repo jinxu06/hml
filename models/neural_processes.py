61f2d7ba4c6891f275c040ec70139e992cf19e7c
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-05 15:49:23 +0800

    sync py code

--- models/neural_processes.py
+++ models/neural_processes.py
@@ -215,6 +215,8 @@ def omniglot_conv_conditional_decoder(inputs, z, num_classes, nonlinearity=None,
             print(int_shape(outputs))
             print(int_shape(z))
             z = tf.tile(tf.reshape(z, [-1, 1, 1, int_shape(z)[-1]]), multiples=tf.stack([1]+int_shape(outputs)[1:3]+[1]))
+            print(int_shape(outputs))
+            print(int_shape(z))
             outputs = tf.concat([outputs, z], axis=-1)
             for _ in range(2):
                 outputs = conv2d(outputs, num_filters, filter_size=filter_size, stride=stride, pad="SAME")
