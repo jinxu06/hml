f071a40d3f02d387872f57bef3e2801af4b6e0ac
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-06 22:46:01 +0800

    sync py code

--- run_miniimagenet_np.py
+++ run_miniimagenet_np.py
@@ -50,7 +50,7 @@ for i in range(args.nr_model):
         model(models[i], **model_opt)
 
 #tags = ["test", 'small-period']
-tags = ["test"]
+tags = ["CNP"]
 
 learner = NPLearner(session=None, parallel_models=models, optimize_op=None, train_set=train_set, eval_set=val_set, variables=tf.trainable_variables(), lr=args.learning_rate, device_type=args.device_type, tags=tags, cdir=checkpoint_dir, rdir=result_dir)
 
