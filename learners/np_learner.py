4118fa6ef9a3a9b78da303817552d9d0bda4c1e6
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-06 11:28:03 +0800

    sync py code

--- learners/np_learner.py
+++ learners/np_learner.py
@@ -90,8 +90,8 @@ class NPLearner(MetaLearner):
                 m.is_training: True,
             })
 
-            #v = self.get_session().run(m.r[0], feed_dict=feed_dict)
-            #print(v)
+            v = self.get_session().run(m.r[0], feed_dict=feed_dict)
+            print(v)
 
 
     def visualise_1d(self, save_name):
