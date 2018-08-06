747928efcf3502c415b17dc7b34f5b1d12ce13e8
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-06 12:07:58 +0800

    sync py code

--- models/conditional_neural_processes.py
+++ models/conditional_neural_processes.py
@@ -77,7 +77,7 @@ class ConditionalNeuralProcess(object):
                     r_c = self.sample_encoder(self.X_c, self.y_c, self.r_dim, self.num_classes)
                     if self.task_type == 'classification':
                         self.r = self.aggregator(r_c, self.y_c, self.z_dim)
-                    self.outputs = self.conditional_decoder(self.X_t, self.r, self.num_classes)
+                    self.outputs = self.conditional_decoder(self.X_t, self.y_t, self.r, self.num_classes) ##
 
     def _loss(self):
         self.nll = self.error_func(self.y_t, self.outputs)
@@ -204,7 +204,7 @@ def omniglot_conv_encoder(X, y, r_dim, num_classes, is_training, nonlinearity=No
 
 
 @add_arg_scope
-def omniglot_conv_conditional_decoder(inputs, z, num_classes, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, is_training=False, counters={}):
+def omniglot_conv_conditional_decoder(inputs, y, z, num_classes, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, is_training=False, counters={}):
     name = get_name("omniglot_conv_conditional_decoder", counters)
     print("construct", name, "...")
     with tf.variable_scope(name):
@@ -228,6 +228,9 @@ def omniglot_conv_conditional_decoder(inputs, z, num_classes, nonlinearity=None,
             outputs = tf.reshape(outputs, [-1, np.prod(int_shape(outputs)[1:])])
             z = tf.tile(z, tf.stack([bsize, 1]))
             outputs = tf.concat([outputs, z], axis=-1)
+
+            outputs = tf.concat([outputs, y], axis=-1)
+
             outputs = dense(outputs, num_filters)
             y = dense(outputs, num_classes, nonlinearity=None, bn=False)
             return y
