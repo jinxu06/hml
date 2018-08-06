f9bd2c70ce97a62a568abe5f06a1d3a80480961b
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-06 14:54:53 +0800

    sync py code

--- learners/np_learner.py
+++ learners/np_learner.py
@@ -89,7 +89,7 @@ class NPLearner(MetaLearner):
                 m.y_t: y_t_value,
                 m.is_training: True,
             })
-            v = self.get_session().run(m.r, feed_dict=feed_dict)
+            v = self.get_session().run(m.r[0], feed_dict=feed_dict)
             l = len(v)
             print(v[:l//2])
             print(v[l//2:])
