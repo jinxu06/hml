c4f902799474058a9a48672c3109fec44f831a57
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-11 16:28:52 +0800

    sync py code

--- run_1d_mip.py
+++ run_1d_mip.py
@@ -50,7 +50,7 @@ for i in range(args.nr_model):
         model(models[i], **model_opt)
 
 
-tags = ["test", "1step"]
+tags = ["test1"]
 # save_dir = "/data/ziz/jxu/neural_processes/test-{0}".format(args.dataset_name)
 learner = MIPLearner(session=None, parallel_models=models, optimize_op=None, train_set=train_set, eval_set=val_set, variables=tf.trainable_variables(), lr=args.learning_rate, device_type=args.device_type, tags=tags, cdir=checkpoint_dir, rdir=result_dir)
 
@@ -74,7 +74,7 @@ with tf.Session(config=config) as sess:
         "eval_samples": 1000,
         "meta_batch": args.nr_model,
         "gen_num_shots": partial(np.random.randint, low=1, high=20),
-        "gen_test_shots": partial(np.random.randint, low=1, high=10),
+        "gen_test_shots": partial(np.random.randint, low=1, high=20),
         "load_params": args.load_params,
     }
 
