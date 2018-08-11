bffdf94b6971a3b46d72140e217c4200d466c4d8
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-11 12:13:07 +0800

    sync py code

--- models/mcmc_implicit_processes.py
+++ models/mcmc_implicit_processes.py
@@ -68,10 +68,11 @@ class MCMCImplicitProcess(object):
         with arg_scope([self.conditional_decoder, self.sample_encoder, self.aggregator], **default_args):
             self.scope_name = get_name("mcmc_implicit_process", self.counters)
             with tf.variable_scope(self.scope_name):
-                r_c = self.sample_encoder(self.X_c, self.y_c, self.r_dim, self.num_classes, bn=False)
-                # z = self.aggregator(r_c, self.z_dim, bn=False)
-                self.z_mu_pos, self.z_log_sigma_sq_pos = self.aggregator(r_c, self.z_dim, bn=False)
-                z = gaussian_sampler(self.z_mu_pos, tf.exp(0.5*self.z_log_sigma_sq_pos))
+                # r_c = self.sample_encoder(self.X_c, self.y_c, self.r_dim, self.num_classes, bn=False)
+                # # z = self.aggregator(r_c, self.z_dim, bn=False)
+                # self.z_mu_pos, self.z_log_sigma_sq_pos = self.aggregator(r_c, self.z_dim, bn=False)
+                # z = gaussian_sampler(self.z_mu_pos, tf.exp(0.5*self.z_log_sigma_sq_pos))
+                z = tf.get_variable('z', shape=[1,self.z_dim], dtype=tf.float32, trainable=True, initializer=kernel_initializer, regularizer=kernel_regularizer)
                 outputs  = self.conditional_decoder(self.X_c, z, reuse=False, counters={})
                 self.outputs_sqs = [self.conditional_decoder(self.X_t, z, reuse=True, counters={})]
                 for k in range(1, max(self.inner_iters, self.eval_iters)+1):
