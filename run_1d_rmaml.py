d4212a485a6a03ccd09885c1e9f0ee38128fadeb
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-12 14:01:31 +0800

    sync py code

--- run_1d_rmaml.py
+++ run_1d_rmaml.py
@@ -70,8 +70,8 @@ with tf.Session(config=config) as sess:
         "save_interval": args.save_interval,
         "eval_samples": 1000,
         "meta_batch": args.nr_model,
-        "num_shots": partial(np.random.randint, low=10, high=11),
-        "test_shots": partial(np.random.randint, low=20, high=21),
+        "gen_num_shots": partial(np.random.randint, low=10, high=11),
+        "gen_test_shots": partial(np.random.randint, low=20, high=21),
         "load_params": args.load_params,
     }
     if args.user_mode == 'train':
