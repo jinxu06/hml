0562fe7f0ef5b43495ff95c63a1eb9a8fbf932f4
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-13 10:47:20 +0800

    sync py code

--- run_test.py
+++ run_test.py
@@ -15,8 +15,8 @@ from misc.estimators import estimate_kld, compute_gaussian_kld
 # initializer = tf.global_variables_initializer()
 # saver = tf.train.Saver()
 
-bsize_x = 1e5
-bsize_y = 1e5
+bsize_x = 1000
+bsize_y = 1000
 d = 16
 
 x_ph = tf.placeholder(dtype=tf.float32, shape=[bsize_x, d])
