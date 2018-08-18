1daab6131a3b53efac6975e2456d237f99845b6c
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-18 12:12:45 +0800

    sync py code

---
+++ data/celeba.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import random
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import misc.helpers as helpers
+
+
+def read_imgs(dir, limit=-1):
+    filenames = []
+    for entry in os.scandir(dir):
+        if limit==0:
+            break
+        if not entry.name.startswith('.') and entry.is_file():
+            limit -= 1
+            filenames.append(entry.name)
+    filenames = sorted(filenames)
+    imgs = np.array([np.array(Image.open(os.path.join(dir, filename))) for filename in filenames]).astype(np.uint8)
+    return imgs
+
+
+def load(data_dir="/data/ziz/not-backed-up/jxu/CelebA", subset='train', size=64, limit=-1):
+    if subset in ['train', 'valid', 'test']:
+        #trainx = np.load(os.path.join(data_dir, "img_cropped_celeba.npz"))['arr_0'][:200000, :, :, :]
+        trainx = read_imgs(os.path.join(data_dir, "celeba{0}-{1}-new".format(size, subset)), limit=limit)
+        trainy = np.ones((trainx.shape[0], ))
+        return trainx, trainy
+    else:
+        raise NotImplementedError('subset should be either train, valid or test')
+
+
+class CelebA(object):
+
+    def __init__(self, data_dir, which_set, dataset_name="celeba"):
+        self.data_dir = data_dir
+        self.which_set = which_set
+        self.dataset_name = dataset_name
+
+        if which_set == 'train':
+            self.images = load(self.data_dir, subset=which_set, size=32, limit=10000)
+        elif which_set == 'val':
+            self.images = load(self.data_dir, subset=which_set, size=32, limit=200)
+        elif which_set == 'test':
+            self.images = load(self.data_dir, subset=which_set, size=32, limit=200)
+        else:
+            raise Exception("unknown {0}".format(which_set))
+
+    def sample(self, num):
+        tasks = []
+        idxs = np.random.choice(len(self.images), size=num, replace=False).astype(np.int32)
+        for i in idx:
+            tasks.append(self.images[i])
+        return tasks 
+
+
+class FaceCurve(object):
+
+    def __init__(self, image):
+        self.image = image
+        self.image = np.mean(self.image.astype(np.float32), axis=-1)
+
+    def sample(self, num_pixels):
+        pass
--- run_test.py
+++ run_test.py
@@ -9,55 +9,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python import debug as tf_debug
 
-from misc.estimators import estimate_kld, compute_gaussian_kld
+
+from data.celeba import CelebA
 
 
-# initializer = tf.global_variables_initializer()
-# saver = tf.train.Saver()
-
-bsize_x = 1000
-bsize_y = 1000
-d = 1
-
-x_ph = tf.placeholder(dtype=tf.float32, shape=[bsize_x, d])
-y_ph = tf.placeholder(dtype=tf.float32, shape=[bsize_y, d])
-kld = estimate_kld(x_ph, y_ph)
-
-
-z_mu_ph = tf.placeholder(dtype=tf.float32, shape=[d])
-z_log_sigma_sq_ph = tf.placeholder(dtype=tf.float32, shape=[d])
-ckld = compute_gaussian_kld(z_mu_ph, z_log_sigma_sq_ph)
-
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-with tf.Session(config=config) as sess:
-    # sess.run(initializer)
-
-    x = np.random.normal(0.0, 1.0, size=(bsize_x, d))
-    y = np.random.normal(0.0, 1.0, size=(bsize_y, d))
-    z = np.random.normal(0.5, 1.5, size=(bsize_y, d))
-
-    feed_dict = {
-        x_ph: x,
-        y_ph: y
-    }
-    print(sess.run(kld, feed_dict=feed_dict))
-
-    feed_dict = {
-        x_ph: x,
-        y_ph: z,
-    }
-    print(sess.run(kld, feed_dict=feed_dict))
-
-
-    feed_dict = {
-        z_mu_ph: np.zeros((d,)),
-        z_log_sigma_sq_ph:np.zeros((d,)),
-    }
-    print(sess.run(ckld, feed_dict=feed_dict))
-
-    feed_dict = {
-        z_mu_ph: np.ones((d,)) * 0.5,
-        z_log_sigma_sq_ph:np.ones((d,)) * 2.*np.log(1.5),
-    }
-    print(sess.run(ckld, feed_dict=feed_dict))
+celeba = CelebA(data_dir, 'train')
+pritn(celeba.sample(10))
