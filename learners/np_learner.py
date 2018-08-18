e960cc9229c12df744534f365c5f4ba1e87bf1a1
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-18 16:23:31 +0800

    sync py code

--- learners/np_learner.py
+++ learners/np_learner.py
@@ -129,7 +129,7 @@ class NPLearner(MetaLearner):
             ops, feed_dict = m.predict(X_c_value, y_c_value, X_value)
             y_hat = self.session.run(ops, feed_dict=feed_dict)[0]
             img = sampler.show(X_value, y_value)
-            print(img.shape)
+            ax.imshow(img)
 
         fig.savefig(save_name)
         plt.close()
