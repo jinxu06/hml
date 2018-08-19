da3aea1b16f4b49a0df6af9b377de210b78a12ac
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-19 12:01:18 +0800

    sync py code

--- models/ldvi_processes.py
+++ models/ldvi_processes.py
@@ -71,11 +71,11 @@ class LangevinDynamicsVIProcess(object):
             with tf.variable_scope(self.scope_name):
                 # log p(x, z)
                 y_sigma = .01
-                z_pr_scale = 100.
+                z_pr_scale = 1.
                 # loss_func = lambda z, o, y, beta: - (tf.reduce_sum(tf.distributions.Normal(loc=0., scale=y_sigma).log_prob(y-o)) \
                 # + beta * tf.reduce_sum(tf.distributions.Normal(loc=0., scale=z_pr_scale).log_prob(z)))
 
-                loss_func = lambda z, o, y, beta: self.error_func(y, o) / (2*y_sigma**2)
+                loss_func = lambda z, o, y, beta: self.error_func(y, o) / (2*y_sigma**2) + beta * tf.reduce_sum(tf.distributions.Normal(loc=0., scale=z_pr_scale).log_prob(z))
                 #
                 num_c = tf.shape(self.X_c)[0]
                 X_ct = tf.concat([self.X_c, self.X_t], axis=0)
