277567260b7ca93cbc5f0eebc4dd1712c3294c53
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-20 11:29:49 +0800

    sync py code

--- models/neural_processes.py
+++ models/neural_processes.py
@@ -94,6 +94,7 @@ class NeuralProcess(object):
                         raise Exception("unknown user_mode")
                     z = (1-self.use_z_pr) * z + self.use_z_pr * z_pr
                     self.outputs = self.conditional_decoder(self.X_t, z, self.num_classes)
+                    self.mse = tf.losses.mean_squared_error(self.y_t, self.outputs)
 
     def _loss(self, beta=1., y_sigma=1./np.sqrt(2)):
         self.reg = compute_2gaussian_kld(self.z_mu_pr, self.z_log_sigma_sq_pr, self.z_mu_pos, self.z_log_sigma_sq_pos)
@@ -124,7 +125,7 @@ class NeuralProcess(object):
         }
         if self.task_type == 'classification':
             return [self.loss, accuracy(self.y_t, self.pred_func(self.outputs))], feed_dict
-        return [self.loss], feed_dict
+        return [self.loss, self.mse], feed_dict
 
 @add_arg_scope
 def miniimagenet_conv_encoder(X, y, r_dim, num_classes, is_training, nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, counters={}):
