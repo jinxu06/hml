7d1726d819b08f4e5edab0b0bbbfcd4d3d82baab
Jin Xu <aaron.jin.xu@gmail.com>
2018-08-20 15:59:07 +0800

    sync py code

--- run_2d_ldvi_process.py
+++ run_2d_ldvi_process.py
@@ -57,7 +57,7 @@ for i in range(args.nr_model):
         model(models[i], **model_opt)
 
 #tags = ["test", 'small-period']
-tags = ["langevin", "ldvi", "2d", "regression"]
+tags = ["langevin", "ldvi", "2d", "regression", "more", "iters"]
 learner = LDVI2DLearner(session=None, parallel_models=models, optimize_op=None, train_set=train_set, eval_set=val_set, variables=tf.trainable_variables(), lr=args.learning_rate, device_type=args.device_type, tags=tags, cdir=checkpoint_dir, rdir=result_dir)
 
 initializer = tf.global_variables_initializer()
